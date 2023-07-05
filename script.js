4faac1d7f06d5f1476154dd81cc6aa953a09d36f
joseluismanonrosario <74918414+joseluismanonrosario@users.noreply.github.com>
2023-07-05 14:31:09 -0400

    Agregando la direccion del servicio web que estamos utilizando

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const AS_URL_BASE = "";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyspJw0AFyATgm4aQbYTLw9_xSA93qZIyJxF4Zr8pr_Ny7dAtVJoVessJPr5bAQi449vA/exec";
 
 let url = document.location.href;
 let k = url.substr(url.lastIndexOf("k=")+2);
